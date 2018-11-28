e58ef6d987a7b4e7e79f006edfd0b2ebe74a483b
Ki <kikim1025@gmail.com>
2018-11-28 00:11:24 -0500

    heroku

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost/chat_db', { useNewUrlParser: true });
+var database = "mongodb://user:pass1kweh>@ds031098.mlab.com:31098/heroku_rvfz435q";
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+} else {
+  mongoose.connect(database, { useNewUrlParser: true });
+}
+
+
+//mongoose.connect('mongodb://localhost/chat_db', { useNewUrlParser: true });
 
 require('./routes/apiRoutes')(app);
 
